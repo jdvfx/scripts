64fae03941c915b1705e84d7e7ff475039f2b643
Julien Depredurand <julien.depredurand@gmail.com>
2022-06-19 20:03:13 -0700

    added clean_file_names

---
+++ clean_file_names.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+# cleanup file names, remove spaces and special characters
+# only allow letters, numbers, dot, dash, underscore
+# replace anything else with underscore
+
+import os
+import re
+import glob
+import sys
+
+outputString =""
+
+for i in glob.glob("*"):
+    b = re.sub('[^a-zA-Z0-9._-]', "_", i)
+    if i!=b:
+        c = "'"+i+"' '"+b+"'\n"
+        os.system('mv '+c)
+        outputString+= "'"+i+"' '"+b+"'\n"
+
+f = open('_file-name-clean.log','w')
+# write a lof of the renamed files, in case something is messed up.
+
+f.write(outputString)
+f.close()
+
+
+
+
+
