1c5539cee1dc31d59af03cf54dbe589e3b18fe38
jdvfx <ju.dep@zoho.com>
2024-05-19 23:22:48 -0700

    cleanup, added nvim config line

--- projects_symlinks.py
+++ projects_symlinks.py
@@ -4,21 +4,30 @@ import os,glob,sys,re
 # don't symlink .lock files and target dirs
 # also create an unlink.sh script to remove all symlinks in each project
 
+"""
+>>> add to nvim lua config <<<
+
+-- go to my projects (telescope find_files)
+vim.keymap.set('n', '<leader>o', ':lua require("telescope.builtin").find_files{cwd="/home/bunker/projects/aliases/",follow=true,search_file="*.*"}<cr>')
+
+"""
+
 projects_dir = "/home/bunker/projects/"
 aliases_dir = "/home/bunker/projects/aliases/"
 
 args = sys.argv
 if len(args)!=3:
-    print("ERROR: needs path to project to symlink and alias_name");
+    print("ERROR: needs project_name to symlink and alias_name")
+    print("eg: python projects_symlinks.py tigershark3 ts3")
     quit()
 else:
     project = projects_dir + args[1];
     alias_name = args[2];
     project_name = project.split("/")[-1];
     project_alias_dir = aliases_dir+alias_name
-    s = "mkdir -p "+ project_alias_dir
-    print(s)
-    os.system(s)
+    command = f"mkdir -p {project_alias_dir}"
+    os.system(command)
+    print(command)
 
     with open(project_alias_dir+"/unlink.sh","w") as f:
         f.write('ls | xargs -I % unlink %')
@@ -29,10 +38,9 @@ else:
             pass
         else:
             symlink = re.sub(project,project_alias_dir,i)
-            s = "ln -s "+i+" "+symlink
-            print(s)
-            os.system(s)
-
+            command = f"ln -s {i} {symlink}"
+            os.system(command)
+            print(command)
 
 
 
