1865c4d22e57d2567bd591411118533d53fae7b1
jdvfx <ju.dep@zoho.com>
2024-12-19 15:38:48 -0800

    cleanup

--- nordvpnkillswitch.sh
+++ nordvpnkillswitch.sh
@@ -1,9 +1,14 @@
+#!/bin/sh
+# -------------
+# nordvpn killswitch
+# if vpn disconnected, kill transmission
+# -------------
 while true
-   do
-      if [[ $(nordvpn status | wc -l) -eq 1 ]]; then
-         echo "KILL TRANSMISSION"
-         killall -9 transmission-gtk
-         exit
-      fi
-   sleep 5
+  do
+    if [ $(nordvpn status | wc -l) -eq 1 ]; then
+      echo "KILL TRANSMISSION"
+      killall -9 transmission-gtk
+      exit
+    fi
+  sleep 5
 done
